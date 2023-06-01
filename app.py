6ed7c81fff3f3cea47e98cfd1746a6355039d61d
Rahul Maligi <maligir@utexas.edu>
2023-06-01 14:09:36 -0500

    added facial verification and socket connection

--- app.py
+++ app.py
@@ -1,229 +1,163 @@
+from ctypes import sizeof
+from pickle import FALSE
+from matplotlib import pyplot
+from PIL import Image
+from numpy import asarray
+from scipy.spatial.distance import cosine
+from mtcnn.mtcnn import MTCNN
+from keras_vggface.vggface import VGGFace
+from keras_vggface.utils import preprocess_input
 import cv2
-import dlib
-import face_recognition
-import pickle5 as pickle
-import os
-import getpass
-import logging
-import argparse
-import re
-import concurrent.futures
-import secrets
-from argon2 import PasswordHasher
-from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, select, LargeBinary
-from sqlalchemy.orm import scoped_session, sessionmaker
-from sqlalchemy.exc import SQLAlchemyError
-from cv2 import cvtColor, COLOR_BGR2GRAY, equalizeHist, CascadeClassifier
-import numpy as np
-from dotenv import load_dotenv
+import os, shutil
+import socket
+# from pyk4a import PyK4A
 
-# Load environment variables
-load_dotenv()
+class FaceVerification:
 
-# Setup logging
-logging.basicConfig(filename='app.log', level=logging.ERROR, 
-                    format='%(asctime)s - %(levelname)s - %(message)s')
-
-# Haar cascades for face detection
-FACE_HAAR_PATH = 'haarcascade_frontalface_default.xml'
-
-
-class CameraHandler:
-    def __init__(self):
-        self.video_capture = cv2.VideoCapture(0)
-
-    def capture_frame(self):
-        ret, frame = self.video_capture.read()
-        return frame
-
-    def close(self):
-        self.video_capture.release()
-
-
-class FaceRecognizer:
-    def __init__(self):
-        self.face_cascade = CascadeClassifier(FACE_HAAR_PATH)
-
-    def get_face_encodings(self, frame):
-        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-        rgb_small_frame = small_frame[:, :, ::-1]
-        return face_recognition.face_encodings(rgb_small_frame)
-
-    def check_face_quality(self, frame):
-        gray = cvtColor(frame, COLOR_BGR2GRAY)
-        gray = equalizeHist(gray)
-        faces = self.face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-        
-        # Checking if a face is detected
-        if len(faces) != 0:
-            return True
+    def __init__(self, default_image='default.jpg'):
+        self.detector = MTCNN()
+        self.model = VGGFace(model='resnet50', include_top=False, input_shape=(224, 224, 3), pooling='avg')
+        self.defaultFaces = self.extract_face(default_image)
+        self.curWidth = 0
+        self.curHeight = 0
+        self.origWidth = 0
+        self.origHeight = 0
+        self.passPhrase = "pass"
+        self.failPhrase = "fail"
+ 
+    # extract a single face from a given photograph
+    def extract_face(self, filename, required_size=(224, 224)):
+        # load image from file
+        pixels = pyplot.imread(filename)
+        oldImage = Image.fromarray(pixels)
+        self.origWidth = oldImage.width
+        self.origHeight = oldImage.height
+        # detect faces in the image
+        results = self.detector.detect_faces(pixels)
+        # extract the bounding box from the first face
+        if len(results) == 0:
+            return self.defaultFaces
+        x1, y1, width, height = results[0]['box']
+        x2, y2 = x1 + width, y1 + height
+        # extract the face
+        face = pixels[y1:y2, x1:x2]
+        # resize pixels to the model size
+        image = Image.fromarray(face)
+        self.curWidth = image.width
+        self.curHeight = image.height
+        image = image.resize(required_size)
+        face_array = asarray(image)
+        return face_array
+    
+    # extract faces and calculate face embeddings for a list of photo files
+    def get_embeddings(self, filenames):
+        # extract faces
+        faces = [self.extract_face(f) for f in filenames]
+        # convert into an array of samples
+        samples = asarray(faces, 'float32')
+        # prepare the face for the model, e.g. center pixels
+        samples = preprocess_input(samples, version=2)
+        # perform prediction
+        yhat = self.model.predict(samples)
+        return yhat
+    
+    # determine if a candidate face is a match for a known face
+    def is_match(self, known_embedding, candidate_embedding, thresh=0.4):
+        # calculate distance between embeddings
+        score = cosine(known_embedding, candidate_embedding)
+        if score <= thresh and self.curWidth/self.origWidth > 0.05 :
+            print('>face is a Match (%.3f <= %.3f)' % (score, thresh))
+            return True, 1
         else:
-            return False
+            print('>face is NOT a Match (%.3f > %.3f)' % (score, thresh))
+            return False, 0
 
-
-class User:
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-
-
-class Database:
-    def __init__(self):
-        engine = create_engine(os.getenv("DATABASE_URL"), echo=True)
-        metadata = MetaData()
-        self.users = Table('users', metadata,
-                           Column('id', Integer, primary_key=True),
-                           Column('username', String),
-                           Column('password', String),
-                           Column('encoding', LargeBinary))
-
-        metadata.create_all(engine)
-        self.Session = scoped_session(sessionmaker(bind=engine))
-
-    def check_username_exists(self, username):
+    def clean_images(self, imageName):
         try:
-            s = select([self.users]).where(self.users.c.username == username)
-            session = self.Session()
-            return session.execute(s).fetchone() is not None
-        except SQLAlchemyError as e:
-            logging.error('Database error during username check: %s', str(e))
-            return False
-        finally:
-            session.remove()
+            if os.path.isfile(imageName) or os.path.islink(imageName):
+                        os.unlink(imageName)
+        except Exception as e:
+            print(e)
 
-    def add_user(self, user, face_encoding):
-        ph = PasswordHasher()
-        hashed_password = ph.hash(user.password)
-        hashed_face_encoding = ph.hash(pickle.dumps(face_encoding))
-        try:
-            ins = self.users.insert().values(username=user.username, password=hashed_password,
-                                             encoding=hashed_face_encoding)
-            session = self.Session()
-            session.execute(ins)
-            session.commit()
-        except SQLAlchemyError as e:
-            logging.error('Database error during user registration: %s', str(e))
-            return False
-        finally:
-            session.remove()
-            # Securely delete sensitive data
-            del hashed_password
-            del hashed_face_encoding
-        return True
+    def clean_folder(self, folder):
+        for filename in os.listdir(folder):
+            file_path = os.path.join(folder, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+            except Exception as e:
+                print('Failed to delete %s. Reason: %s' % (file_path, e))
+    
+    def write_result(self, trueOrNot, filename):
+        f = open(filename, "r")
+        fileContents = f.read()
+        f.close()
+        if(fileContents == self.passPhrase and trueOrNot == False):
+            f = open(filename, "w")
+            f.write(self.failPhrase)
+        elif(fileContents == self.failPhrase and trueOrNot == True):
+            f = open(filename, "w")
+            f.write(self.passPhrase)
 
+    def send_socket_message(self, message: str):
+        # Create a socket object 
+        serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+        serversocket.settimeout(2)
+        # Bind to the port 
+        serversocket.bind(('localhost', 12345))                                  
+        # Start listening for client connections
+        serversocket.listen(5)
+        # Accept a connection from the client
+        clientsocket, addr = serversocket.accept()       
+        # Send the message
+        clientsocket.send(message.encode('utf-8'))  
+        # Close the connection
+        clientsocket.close()
+        serversocket.close()
 
-class CLI:
-    def get_args(self):
-        parser = argparse.ArgumentParser(description='Register a new user.')
-        parser.add_argument('--username', required=True, help='Username for the new user.')
-        parser.add_argument('--password', required=True, help='Password for the new user.')
-        args = parser.parse_args()
-        return args
+    def run(self):
+        # Initialize camera
+        vc = cv2.VideoCapture(0)
+        # Try to read in first frame
+        if vc.isOpened():
+            rval, frame = vc.read()
+        else:
+            rval = False
+        filenames = []
+        frameCounter = 0
+        fileCounter, match = (0, 0)
+        max_iterations = 10000  # Set your max limit
+        iteration_count = 0
+        while match == 0 and iteration_count < max_iterations and rval:
+            iteration_count += 1
+            # Get frame from camera and wait a few milliseconds
+            cv2.imshow("preview", frame)
+            frame = vc.read()[1]
+            if(frameCounter%120 == 0):
+                if(fileCounter > 1):
+                    fileCounter = 1
+                filename = "img" + str(fileCounter) + ".jpg"
+                cv2.imwrite(filename, frame)
+                # frame.save(filename)
+                filenames.append(filename)
+                if(len(filenames) > 2):
+                    filenames.pop(1)
+                print(filenames)
+                embeddings = self.get_embeddings(filenames)
+                trueOrNot, match = self.is_match(embeddings[0], embeddings[fileCounter])
+                # Send a socket message when the face is recognized
+                if trueOrNot:
+                    self.send_socket_message('Face recognized!')
+                fileCounter += 1
+                fileCounter += 1
+            #update frame counter
+            frameCounter+=1
+        # Clean up and Destroy all windows and files
+        cv2.destroyAllWindows()
+        self.clean_images("img0.jpg")
+        self.clean_images("img1.jpg")
 
-
-def validate_username(username):
-    # Only allow alphanumeric characters and underscores in username
-    return re.match(r'^\w+$', username) is not None
-
-
-def validate_password(password):
-    # Enforce minimum password length
-    return len(password) >= 8
-
-
-def register_new_user(args):
-    user = User(args.username, args.password)
-
-    if not validate_user(user):
-        return
-
-    camera = CameraHandler()
-    frame = capture_frame(camera)
-
-    if not validate_frame(frame):
-        return
-
-    face_encodings = calculate_face_encodings(frame)
-
-    if face_encodings is None:
-        return
-
-    db = Database()
-    if not add_user_to_db(db, user, face_encodings[0]):
-        return
-
-    print(f'User {user.username} registered successfully.')
-
-
-def validate_user(user):
-    if not validate_username(user.username):
-        print('Username can only contain alphanumeric characters and underscores.')
-        return False
-
-    if not validate_password(user.password):
-        print('Password must be at least 8 characters long.')
-        return False
-
-    db = Database()
-
-    # Check if username already exists
-    if db.check_username_exists(user.username):
-        print('This username is already registered. Please choose a different one.')
-        return False
-
-    return True
-
-
-def capture_frame(camera):
-    # Take a photo
-    print('Taking photo in 3 seconds...')
-    cv2.waitKey(3000)
-    frame = camera.capture_frame()
-    camera.close()
-    return frame
-
-
-def validate_frame(frame):
-    recognizer = FaceRecognizer()
-
-    # Check face quality
-    if not recognizer.check_face_quality(frame):
-        print('The quality of the face in the photo is not sufficient. Please try again.')
-        return False
-
-    return True
-
-
-def calculate_face_encodings(frame):
-    recognizer = FaceRecognizer()
-    # Calculate face encoding
-    face_encodings = recognizer.get_face_encodings(frame)
-    if not face_encodings:
-        print('No face was detected in the photo. Please try again.')
-        return None
-
-    return face_encodings
-
-
-def add_user_to_db(db, user, face_encoding):
-    # Add user to database
-    if not db.add_user(user, face_encoding):
-        print('User registration failed due to a database error.')
-        return False
-
-    return True
-
-
-def main():
-    cli = CLI()
-    args = cli.get_args()
-
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        futures = {executor.submit(register_new_user, args)}
-        for future in concurrent.futures.as_completed(futures):
-            if future.exception() is not None:
-                logging.error('Exception during user registration: %s', str(future.exception()))
-
-
-if __name__ == "__main__":
-    main()
+app = FaceVerification()
+app.run()
